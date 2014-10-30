56fff8ba70a6cc38c5024baa69f128d99bad24bb
Ruslan Absalyamov <ruslan.absalyamov@gmail.com>
2014-10-30 22:25:39 +0300

    DataSource configured with support openshift environment.

---
+++ src/main/java/com/arusland/bozor/config/DataSourceConfig.java
@@ -0,0 +1,57 @@
+package com.arusland.bozor.config;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String userName;
+    @Value("${spring.datasource.password}")
+    private String pwd;
+
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+
+        String username = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+        String password = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+        String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+        String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+        String databaseName = System.getenv("OPENSHIFT_APP_NAME");
+        String jdbcUrl;
+
+        if (host != null || databaseName != null || port != null) {
+            jdbcUrl = "jdbc:mysql://" + host + ":" + port + "/" + databaseName
+                    + "?useUnicode=yes&characterEncoding=UTF-8";
+        } else {
+            username = userName;
+            password = pwd;
+            jdbcUrl = url;
+        }
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(jdbcUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setTestOnBorrow(true);
+        dataSource.setTestOnReturn(true);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTimeBetweenEvictionRunsMillis(3600000);
+        dataSource.setNumTestsPerEvictionRun(3);
+        dataSource.setMinEvictableIdleTimeMillis(3600000);
+        dataSource.setValidationQuery("SELECT 1");
+
+        return dataSource;
+    }
+}
